058e65487c2c88f42b04877b19e61faeb682b28a
Muhammad Noviyanto <muhnoviyanto815@gmail.com>
2024-12-15 16:21:02 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     domains: ['images.unsplash.com','cdn.dribbble.com']
   }
